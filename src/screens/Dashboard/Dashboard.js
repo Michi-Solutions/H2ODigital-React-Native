cf5101432a1be0f694f56b1eed65bcbb44800313
mayconabe <maycon.abe01@gmail.com>
2022-02-11 13:19:21 -0300

    Added fetch() to get data

---
+++ src/screens/Dashboard/Dashboard.js
@@ -0,0 +1,39 @@
+import React, { Component, useEffect, useState } from 'react';
+import { View, StyleSheet, FlatList, Text } from 'react-native';
+import {decode, encode} from 'base-64'
+
+if (!global.btoa) {
+    global.btoa = encode;
+}
+    
+if (!global.atob) {
+    global.atob = decode;
+}
+
+export default function Dashboard() {
+
+    const [data, setData] = useState([]);
+    const url = "http://h2odigital.com.br/api/dashboard/136"
+    console.log(data);
+
+    useEffect(() => {
+        fetch(url, { 
+            method: 'get', 
+            headers: new Headers({
+                'Authorization': 'Basic '+btoa('mauricio.abe@michisolutions.com.br:Senha123'), 
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }), 
+        })
+          .then((response) => response.json())
+          .then((json) => {
+            setData(Object.values(json))
+          })
+          .catch((error) => console.error(error))
+    }, []);
+
+    return (
+      <View>
+        <Text>{data.leiturasAtuais}</Text>
+      </View>
+    )
+}
