df57542eeddb59865535f06a66dccc07b724a6ea
mayconabe <maycon.abe01@gmail.com>
2022-02-11 13:20:16 -0300

    Added Screen Dashboard

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import { View, StyleSheet } from 'react-native';
+import Dashboard from './src/screens/Dashboard/Dashboard';
 
 export default class App extends Component {
   render() {
     return(
       <View>
-
+        <Dashboard/>
       </View>
     )
   }
