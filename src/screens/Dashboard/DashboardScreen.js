8d6bcd7ea77d07aa84adc18010564eb797c12382
mayconabe <maycon.abe01@gmail.com>
2022-02-14 16:46:21 -0300

    changed from fucntion to class and used props

--- src/screens/Dashboard/DashboardScreen.js
+++ src/screens/Dashboard/DashboardScreen.js
@@ -11,42 +11,52 @@ if (!global.atob) {
     global.atob = decode;
 }
 
-export default function DashboardScreen() {
+export default class DashboardScreen extends Component {
 
-    const [data, setData] = useState([]);
+  constructor(props) {
+    super(props)
+    this.state = {
+      data: ["No data"],
+      nome: ["No data"]
+    }
+  }
+  
+
+  componentDidMount() {
     const url = "http://h2odigital.com.br/api/dashboard/136"
-    // console.log(data);
+  
+    fetch(url, { 
+      method: 'get', 
+      headers: new Headers({
+          'Authorization': 'Basic '+btoa('mauricio.abe@michisolutions.com.br:Senha123'), 
+          'Content-Type': 'application/x-www-form-urlencoded'
+        }), 
+    })
+    .then((response) => response.json())
+    .then((json) => {
+      this.setState({data: Object.values(json), nome: Object.values(json)[0][0].reservatorio.nome})
+      console.log(this.state.data)
+    })
+  }
 
-    useEffect(() => {
-        fetch(url, { 
-            method: 'get', 
-            headers: new Headers({
-                'Authorization': 'Basic '+btoa('mauricio.abe@michisolutions.com.br:Senha123'), 
-                'Content-Type': 'application/x-www-form-urlencoded'
-            }), 
-        })
-          .then((response) => response.json())
-          .then((json) => {
-            setData(Object.values(json))
-          })
-          // .catch((error) => console.error(error))
-    }, []);
-
-    return (
-        
-        <View style={styles.container}>
-          <Text style={styles.welcome}>
-            Bem Vindo, Usuário
-          </Text>
-          <DashboardComponent/>
-      </View>
-    )
+    render() {
+      return (
+          
+          <View style={styles.container}>
+            <Text style={styles.welcome}>
+              Bem Vindo, Usuário
+            </Text>
+            <DashboardComponent nome={this.state.nome} volumeTotal={this.state.data[0][0].volumeMaximoFormatado} percentual={this.state.data[0][0].percentual} ultimaLeitura={this.state.data[0][0].dataUltimaLeituraFormatada} percentualGrafico={this.state.data[0][0].percentual}/>
+        </View>
+      )
+    }
 }
 
 const styles = StyleSheet.create({
   container: {
     height: '100%',
-    backgroundColor: '#EFEFEF'
+    backgroundColor: '#EFEFEF',
+    paddingTop: 25
   },
   welcome: {
     fontWeight: 'bold',
