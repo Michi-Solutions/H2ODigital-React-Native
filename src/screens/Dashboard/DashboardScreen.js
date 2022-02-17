ff97146452e4eb06b7d259959ea0bbe6bfc42cad
marleyabe <marleyabe@gmail.com>
2022-02-17 11:51:05 -0300

    Fixed fetchs

--- src/screens/Dashboard/DashboardScreen.js
+++ src/screens/Dashboard/DashboardScreen.js
@@ -1,5 +1,5 @@
 import React, { Component, useEffect, useState } from 'react';
-import { View, StyleSheet, FlatList, Text } from 'react-native';
+import { View, StyleSheet, FlatList, Text, ActivityIndicator } from 'react-native';
 import {decode, encode} from 'base-64'
 import DashboardComponent from './DashboardComponent'
 
@@ -16,59 +16,75 @@ export default class DashboardScreen extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      data: ["No data"],
+      data: "No data",
       nome: ["No data"],
       edificio: ["No data"],
+      isLoading: true
     }
   }
 
-  componentDidMount() {
-    const {id, ...user} = this.props.route.params
-    console.log(user.email)
-    const urlDashboard = `http://h2odigital.com.br/api/dashboard/${id}`
-    const urlUser = `http://www.h2odigital.com.br/api/estabelecimento/capturar/${id}`
+  async componentDidMount() {
+    while ( this.state.data == "No data" ) {
+      try {
+        const response = await fetch('http://www.h2odigital.com.br/api/estabelecimento/capturar/136',{ 
+        method: 'get', 
+        headers: new Headers({
+            'Authorization': 'Basic '+btoa('mauricio.abe@michisolutions.com.br:Senha123'), 
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'cache-control': 'no-store',
+            'pragma': 'no-cache'
+          }), 
+        });
+        const userInfo = await response.json();
+        this.setState({edificio:userInfo.nome})
+        
 
-    fetch(urlDashboard, { 
-      method: 'get', 
-      headers: new Headers({
-          'Authorization': 'Basic '+btoa('mauricio.abe@michisolutions.com.br:Senha123'), 
-          'Content-Type': 'application/x-www-form-urlencoded'
-        }), 
-    })
-    .then((response) => response.json())
-    .then((json) => {
-      this.setState({data: Object.values(json), nome: Object.values(json)[0][0].reservatorio.nome})
-    })
 
-    fetch(urlUser, { 
-      method: 'get', 
-      headers: new Headers({
-          'Authorization': 'Basic '+btoa('mauricio.abe@michisolutions.com.br:Senha123'), 
-          'Content-Type': 'application/x-www-form-urlencoded'
-        }), 
-    })
-    .then((response) => response.json())
-    .then((json) => {
-      this.setState({edificio: json})
-    })
-  }
+        const dashboard = await fetch('http://h2odigital.com.br/api/dashboard/136',{ 
+        method: 'get', 
+        headers: new Headers({
+            'Authorization': 'Basic '+btoa('mauricio.abe@michisolutions.com.br:Senha123'), 
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'cache-control': 'no-store',
+            'pragma': 'no-cache'
+          }), 
+        });
+        const dashboardRes = await dashboard.json();
+        this.setState({data:Object.values(dashboardRes)})
 
-  
+
+        this.setState({isLoading: false})
+
+
+      } catch(err) {
+        setTimeout(() => {console.log('.')}, 50000);
+      }
+    }
+}
 
     render() {
-      return (
+      if (this.state.isLoading == false){
+        return (
           
           <View style={styles.container}>
             <Text style={styles.welcome}>
-              {this.state.edificio.nome}
+              {this.state.edificio}
             </Text>
-            <DashboardComponent nome={this.state.nome} 
+            <DashboardComponent nome={this.state.data[0][0].reservatorio.nome} 
                                 volumeTotal={this.state.data[0][0].volumeMaximoFormatado} 
                                 percentual={this.state.data[0][0].percentual} 
                                 ultimaLeitura={this.state.data[0][0].dataUltimaLeituraFormatada} 
                                 percentualGrafico={this.state.data[0][0].percentual}/>
-        </View>
-      )
+          </View>
+        )
+      } else {
+        return (
+          <View style={[styles.loader]}>
+            <ActivityIndicator size="large" color="#57B5DB" />
+          </View>
+        )
+      }
+      
     }
 }
 
@@ -84,5 +100,10 @@ const styles = StyleSheet.create({
       fontSize: 24,
       marginLeft: 20,
       marginTop: 15
-  }
+  },
+  loader: {
+    flex: 1,
+    justifyContent: "center",
+    backgroundColor: '#EFEFEF'
+  },
 });
