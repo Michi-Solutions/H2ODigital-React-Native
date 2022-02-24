75c3b773a978f65db62e578ae072ddfcfdd4269d
Marley Abe <52432980+marleyabe@users.noreply.github.com>
2022-02-24 14:30:56 -0300

    Update attemps

--- src/screens/Login/LoginScreen.js
+++ src/screens/Login/LoginScreen.js
@@ -54,7 +54,7 @@ export default class DashboardScreen extends Component {
             this.Login()
             this.setState({tentativas: this.state.tentativas + 1 })
           }
-          if (this.state.tentativas >= 5) {
+          if (this.state.tentativas <= 4) {
             this.setState({formError: "Usuário ou senha incorretos"})
           }
         }
